8258346ec355898090d91379700cc085fea774f3
Ket Hao <haoketys@gmail.com>
2023-12-21 12:24:50 +0700

    a

--- backend/index.js
+++ backend/index.js
@@ -39,6 +39,8 @@ cartRoutes(app);
 blogRoute(app);
 payment(app);
 app.get("/", (req, res) => {
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
   res.send("Hello😁 Welcome ecommerce API!");
 });
 
