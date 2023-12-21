947cac617443020a51d74f683b55e3909ac47c99
Ket Hao <haoketys@gmail.com>
2023-12-21 12:16:29 +0700

    a

--- backend/index.js
+++ backend/index.js
@@ -27,9 +27,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
-app.use(express.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
+app.use(bodyParser.json()); app.use(bodyParser.urlencoded({ extended: true }));
 // my-routes
 authRoutes(app);
 categoryRoutes(app);
