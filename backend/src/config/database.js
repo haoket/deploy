6e692d3d9d95200dbf027d5aada90a61aa9cf84b
Ket Hao <haoketys@gmail.com>
2023-12-21 14:08:00 +0700

    a

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -2,10 +2,14 @@ import mysql from 'mysql';
 
 const createDatabaseConnection = () => {
     const connection = mysql.createConnection({
-        host: process.env.HOST,
-        user: process.env.USER,
-        password: process.env.PASSWORD,
-        database: process.env.DATABASE
+        // host: process.env.HOST,
+        // user: process.env.USER,
+        // password: process.env.PASSWORD,
+        // database: process.env.DATABASE
+        host: "mysql-beauty.alwaysdata.net",
+        user: "beauty",
+        password: "qwerty.123456",
+        database: "beauty_shop"
     });
 
     return connection;
