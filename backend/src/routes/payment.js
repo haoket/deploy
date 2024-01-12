e69d19208e447958e113ee1f570a004472135e6b
Ket Hao <haoketys@gmail.com>
2024-01-12 17:34:19 +0700

    add id paypal

--- backend/src/routes/payment.js
+++ backend/src/routes/payment.js
@@ -8,7 +8,7 @@ const payment = (app) => {
     app.route("/payment/config").get((req, res) => {
         return res.status(200).json({
             status: 'ok',
-            data: process.env.CLIENT_ID
+            data: "AaSUalQU7e902SaApX_Ja9DtcJPw9Y3sR6vNQa6sKKP1pAkbN4WuKIW2jzyEyHTsvXX5-ICgWFweEU44"
         })
     })
 }
