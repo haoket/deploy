655d9b15667b9d42fbeb04a3e4d914604d63c704
Ket Hao <haoketys@gmail.com>
2024-01-12 14:51:58 +0700

    áđâsd

--- Client/src/user/components/profile/Profile.jsx
+++ Client/src/user/components/profile/Profile.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import { apiDomain } from '../../../utils/utilsDomain';
 import { Context } from '../../../context/Context';
 import axios from 'axios';
-import './profile.css'
+import './Profile.css'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Suggest from '../product/Suggest';
